8c7466f83db55ed9a674220014da4b70b698924b
Stephanie Lawal <stephanie.1.lawal@kcl.ac.uk>
2022-09-23 23:53:58 +0100

    Completed median activity

--- median.py
+++ median.py
@@ -1,12 +1,21 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def median(nums):
+    if len(nums)%2 == 0:
+        med = (nums[int(len(nums)/2)] + nums[int((len(nums)/2)-1)])/2
+    else:
+        med = nums[int((len(nums)-1)/2)]
+    return(med)
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+median(numbers)
